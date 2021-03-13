0bafe961dbcdea45bb25198f1de5e99c5683c0c2
Philipe Gama <philipegama97@gmail.com>
2021-03-12 22:24:55 -0400

    update em calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,33 +1,40 @@
 def soma(numero1, numero2):
-	print(numero1 + numero2)
+    return numero1 + numero2
+
+
 def subtracao(numero1, numero2):
-	print(numero1 - numero2)
+    return numero1 - numero2
+
 
 def multiplicacao(numero1, numero2):
-	print(numero1 * numero2)
+    return numero1 * numero2
+
 
 def divisao(numero1, numero2):
-	print(numero1 / numero2)
+    return numero1 / numero2
+
 
 def calculo(operacao, numero1, numero2):
-	if operacao == '+':
-		soma(numero1, numero2)
-	elif operacao == '-':
-		subtracao(numero1,numero2)
-	elif operacao == '*':
-		multiplicacao(numero1,numero2)
-	elif operacao == '/':
-		divisao(numero1,numero2)
-	else:
-		print('Operacao invalido')
+    if operacao == '+':
+        return soma(numero1, numero2)
+    elif operacao == '-':
+        return subtracao(numero1, numero2)
+    elif operacao == '*':
+        return multiplicacao(numero1, numero2)
+    elif operacao == '/':
+        return divisao(numero1, numero2)
+
 
 def calculadora():
-	numero1 = int(input('Digite o primeiro numero: '))
-	numero2 = int(input('Digite o segundo numero: '))
-	operacao = input ('Digite a operação [+ - * /]: ')
+    print("CALCULADORA")
+    numero1 = int(input('Digite o primeiro numero: '))
+    numero2 = int(input('Digite o segundo numero: '))
+    operacao = input('Digite a operação [+ - * /]: ')
 
-	calculo(operacao,numero1,numero2)
-        
-	input ('...')
+    resultado = calculo(operacao, numero1, numero2)
+    print('Resultado: '+str(resultado))
+
+    input('...')
+
 
 calculadora()
